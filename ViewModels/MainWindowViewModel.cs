8317cfd6e827f47802b34a7dbf1c6ac40c620e81
fqye <fqye@lanyusan.com>
2022-11-20 16:43:50 +0800

    Initial

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,88 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+using System.Threading;
+using ReactiveUI;
+
+
+namespace Reative.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase,IActivatableViewModel
+    {
+
+        private string _greeting = "Hello world";
+        public string Greeting {
+            get => _greeting;
+            set => this.RaiseAndSetIfChanged(ref _greeting, value);
+        }
+        public ViewModelActivator Activator { get; }
+
+        public MainWindowViewModel()
+        {
+            Activator = new ViewModelActivator();
+            this.WhenActivated((CompositeDisposable disposables) =>
+            {
+
+                    // Just log the ViewModel's activation
+                    // https://github.com/kentcb/YouIandReactiveUI/blob/master/ViewModels/Samples/Chapter%2018/Sample%2004/ChildViewModel.cs
+                    Console.WriteLine(
+                        $"[vm {Thread.CurrentThread.ManagedThreadId}]: " +
+                        "ViewModel activated");
+
+                    // Asynchronously generate a new greeting message every second
+                    // https://reactiveui.net/docs/guidelines/framework/ui-thread-and-schedulers
+                    Observable
+                        .Timer(
+                            TimeSpan.FromMilliseconds(100), // give the view time to activate
+                            TimeSpan.FromMilliseconds(1000),
+                            RxApp.MainThreadScheduler)
+                        .Take(Traits.Length)
+                        .Do(
+                            t => {
+                                var newGreeting = $"Hello, {Traits[t % Traits.Length]} world !";
+                                Console.WriteLine(
+                                    $"[vm {Thread.CurrentThread.ManagedThreadId}]: " +
+                                    $"Timer Observable -> " +
+                                    $"Setting greeting to: \"{newGreeting}\"");
+                                Greeting = newGreeting;
+                            },
+                            () =>
+                                Console.WriteLine(
+                                    "Those are all the greetings, folks! " +
+                                    "Feel free to close the window now...\n"))
+                        .Subscribe()
+                        .DisposeWith(disposables);
+
+                    // Just log the ViewModel's deactivation
+                    // https://github.com/kentcb/YouIandReactiveUI/blob/master/ViewModels/Samples/Chapter%2018/Sample%2004/ChildViewModel.cs
+                    Disposable
+                        .Create(
+                            () =>
+                                Console.WriteLine(
+                                    $"[vm {Thread.CurrentThread.ManagedThreadId}]: " +
+                                    "ViewModel deactivated"))
+                        .DisposeWith(disposables);
+                });
+
+            this
+                .WhenAnyValue(vm => vm.Greeting)
+                .Skip(1) // ignore the initial NullOrEmpty value of Greeting
+                .Do(
+                    greeting =>
+                        Console.WriteLine(
+                            $"[vm {Thread.CurrentThread.ManagedThreadId}]: " +
+                            "WhenAnyValue()   -> " +
+                            $"Greeting value changed to: \"{greeting}\"\n"))
+                .Subscribe();
+        }
+        private static readonly string[] Traits = {
+            "expressive",
+            "clear",
+            "responsive",
+            "concurrent",
+            "reactive"
+        };
+
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,58 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
+using Reative.ViewModels;
+
+namespace Reative.Views
+{
+    public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
+    {
+        public MainWindow()
+        {
+            ViewModel = new MainWindowViewModel();
+
+            // ViewModel's WhenActivated block will also get called.
+            this.WhenActivated(disposables => {
+                // Jut log the View's activation
+                Console.WriteLine(
+                    $"[v  {Thread.CurrentThread.ManagedThreadId}]: " +
+                        "View activated\n");
+
+                // Just log the View's deactivation
+                Disposable
+                    .Create(
+                    () =>
+                    Console.WriteLine(
+                        $"[v  {Thread.CurrentThread.ManagedThreadId}]: " +
+                            "View deactivated"))
+                    .DisposeWith(disposables);
+
+                // https://reactiveui.net/docs/handbook/events/#how-do-i-convert-my-own-c-events-into-observables
+                // Observable
+                //     .FromEventPattern(wndMain, nameof(wndMain.Closing))
+                //     .Subscribe(
+                //     _ => {
+                //         Console.WriteLine(
+                //             $"[v  {Thread.CurrentThread.ManagedThreadId}]: " +
+                //                 "Main window closing...");
+                //     })
+                //     .DisposeWith(disposables);
+
+                // https://reactiveui.net/docs/handbook/data-binding/
+                // Console.WriteLine("==> {0}", ViewModel);
+                this
+                    .OneWayBind(ViewModel, vm => vm.Greeting, v => v.tbGreetingLabel.Text)
+                    .DisposeWith(disposables);
+            });
+            AvaloniaXamlLoader.Load(this);
+            // InitializeComponent();
+        }
+        // private Window wndMain => this.FindControl<Window>("wndMain");
+        // private TextBlock tbGreetingLabel => this.FindControl<TextBlock>("tbGreetingLabel");
+    }
+}
